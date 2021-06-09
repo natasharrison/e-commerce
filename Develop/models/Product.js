95e1b144eadd1fe38425ef5312519ad92391b8f8
natasharrison <natasharrison@gmail.com>
2021-06-08 19:06:21 -0700

    category model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+//  id
+id: {
+  type: DataTypes.INTEGER, 
+  allowNull: false, 
+  primaryKey: true, 
+  autoIncrement: true
+}, 
+
+// product_name
+product_name: {
+  type: DataTypes.STRING, 
+  allowNull: false, 
+}, 
+
+// price
+price: {
+  type: DataTypes.DECIMAL, 
+  allowNull: false, 
+  // Validates that the value is a decimal
+
+}, 
+
+// stock
+stock: {
+  type: DataTypes.INTEGER, 
+  allowNull: false, 
+// Set a default value of 10
+// Validates that the value is numeric
+}, 
+category_id: {
+  type: DataTypes.INTEGER, 
+  // References the category model's id
+}
   },
   {
     sequelize,
--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,19 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+//     id
+
+// Integer
+
+// Doesn't allow null values
+
+// Set as primary key
+
+// Uses auto increment
+
+// tag_name
+
+// String
   },
   {
     sequelize,
